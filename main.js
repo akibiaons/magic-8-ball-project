a430249019c27f56946ac44dd2a60d7ac21c9e9c
Arthur Fernandez <arthur@snooget.com>
2022-11-30 12:56:50 -0800

    added 8 ball code to javascript file

---
+++ main.js
@@ -0,0 +1,5 @@
+let userName = 'Codecunts';
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello');
+
+const userQuestion = 'Is Kanye West going to become the president?';
+console.log(`${userName} has asked the following question --> ${userQuestion}`);
