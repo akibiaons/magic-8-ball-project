f9050a5b5c89a98e2a729ecd3f5ed7ff2fe8d578
Arthur Fernandez <arthur@snooget.com>
2022-12-02 12:41:24 -0800

    Added some index and css, also added the rest of the control flow with Javascript

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
-let userName = 'Codecunts';
-userName ? console.log(`Hello, ${userName}!`) : console.log('Hello');
+let userName = "Flor";
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello");
 
-const userQuestion = 'Is Kanye West going to become the president?';
-console.log(`${userName} has asked the following question --> ${userQuestion}`);
+const userQuestion = "she will write her entire essay today?";
+console.log(`${userName} has asked if ${userQuestion}`);
+//Above is the code for users, below is a random number generator which applies to the response made by thr eightBall variable.
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+switch (randomNumber) {
+  case 0:
+    console.log('It is certain');
+    break;
+  case 1:
+    console.log('It is decidedly so');
+    break;
+  case 2:
+    console.log('Reply hazy try again');
+    break;
+  case 3:
+    console.log('Cannot predict now');
+    break;
+  case 4:
+    console.log('Do not count on it');
+    break;
+  case 5:
+    console.log('My sources say no');
+    break;
+  case 6:
+    console.log('Outlook not so good');
+    break;
+  case 7:
+    console.log('Signs point to yes');
+    break;
+  default:
+    console.log('I am broken right now. Ben?.');
+    break;
+}
+// The code below is ommited due to it not working to serve the randomly generated number with the corresponding prompt...
